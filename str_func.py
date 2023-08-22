0843815d83144f9c95fd4ea70632341b2623aa59
yachanova_tania <tan9l_85@mail.ru>
2023-08-22 18:59:30 +0300

    add func_title

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def func_upper(text):
+    """перевод всех букв в заглавные"""
     return text.upper()
+
+
+def func_title(text):
+    """перевод первых букв в заглавные"""
+    return text.title()
 
